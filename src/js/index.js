5cea365e2ca244b2abab7e1eb1baaf39fa817ca9
CommanderSant <santoslozanocamiloandres@hotmail.com>
2019-09-27 23:48:16 +0000

    Card-Random-Generator

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,20 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let cardNumber = document.querySelector(".number");
+  cardNumber.innerHTML = generateRandomNumber();
+  let suit = generateRandomSuit();
+  document.querySelector(".top").classList.add(suit);
+  document.querySelector(".bottom").classList.add(suit);
 };
+
+function generateRandomNumber() {
+  let values = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
+  let indexValues = Math.floor(Math.random() * values.length);
+  return values[indexValues];
+}
+function generateRandomSuit() {
+  let suits = ["spades", "clubs", "diamond", "hearts"];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
+}
